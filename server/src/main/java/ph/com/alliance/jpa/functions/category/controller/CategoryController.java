ba24aeadeb6b3c5c0476f605e945a6e05f26a552
Cj <tanedochristian1@gmail.com>
2023-04-22 23:44:14 +0800

    edit requestbody

--- server/src/main/java/ph/com/alliance/jpa/functions/category/controller/CategoryController.java
+++ server/src/main/java/ph/com/alliance/jpa/functions/category/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class CategoryController {
 	}
 	
 	@PutMapping("/update/{id}")
-	public void update(@PathVariable Integer id, CategoryModel category) {
+	public void update(@PathVariable Integer id, @RequestBody CategoryModel category) {
 		categoryService.updateCategory(id, category);
 		
 	}
